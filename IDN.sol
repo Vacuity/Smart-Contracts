63ab005b92bc2999bdb30b871700a3ed53af2dba
Sherman Monroe <shermanmonroe@Shermans-MacBook-Air.local>
2019-04-22 23:55:45 -0500

    Stubs and test code
    - added stubs for the four token types
    - added test code for IDN token

---
+++ IDN.sol
@@ -0,0 +1,26 @@
+contract IDN is ERC721Full {
+    using Counter for Counter.Index;
+    Counter.Index private tokenId;
+
+    constructor(
+        string name,
+        string symbol,
+    )
+        ERC721Full(name, symbol)
+        public
+    {}
+
+    function createIDN(
+        string uri,
+        string tokenUri
+    )
+        public
+        returns (bool)
+    {
+
+        uint256 id = uint256(keccak256(abi.encodePacked(uri)));
+        _mint(msg.sender, id);
+        _setTokenURI(id, tokenUri);
+        return true;
+    }
+}
