0da8131bf55dc09f604d18a3c8741076892dbd72
Sherman Monroe <shermanmonroe@Shermans-MacBook-Air.local>
2019-04-25 03:05:38 -0500

    Token logic
    - added logic for VIP-181 token
    - added logic for VIP-180 token

---
+++ VIP-180/VIOS.sol
@@ -0,0 +1,178 @@
+pragma solidity ^0.5.2;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "openzeppelin-solidity/contracts/access/Roles.sol";
+
+/**
+ * @title Capped token
+ * @dev Mintable token with a token cap.
+ */
+contract ERC20Capped {
+    uint256 private _cap;
+
+    constructor (uint256 cap) public {
+        require(cap > 0, "ERC20Capped: cap is 0");
+        _cap = cap;
+    }
+
+    /**
+     * @return the cap for the token minting.
+     */
+    function cap() public view returns (uint256) {
+        return _cap;
+    }
+
+    function _mint(address account, uint256 value) internal {
+        require(totalSupply().add(value) <= _cap, "ERC20Capped: cap exceeded");
+        super._mint(account, value);
+    }
+}
+
+/**
+ * @title VIOS Network Token
+ * @dev 
+ */
+contract VIOS is ERC20, ERC20Detailed, ERC20Capped {
+    using Roles for Roles.Role;
+    Roles.Role private delegates;
+
+
+    uint8 public constant DECIMALS = 18;
+    uint256 public constant INITIAL_SUPPLY = 100000000 * (10 ** uint256(DECIMALS));
+    uint256 public constant MAX_SUPPLY = 500000090 * (10 ** uint256(DECIMALS));
+    unit256 public constant TOKENS_PER_BLOCK = 38059 * (10 ** uint256(DECIMALS - 4));
+
+    string public name = "VIOS Network Token";
+    string public symbol = "VIOS";
+    uint8 public decimals = DECIMALS;
+    uint8 public last_claim_block_number = 0;
+
+
+    mapping(address => uint256) balances;
+    mapping (address => mapping (address => uint256)) internal allowed;
+
+    /**
+    * @dev Total number of tokens in existence
+    */
+    function totalSupply() public view returns (uint256) {
+        return totalSupply;
+    }
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor () public {
+    	_cap = MAX_SUPPLY;
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+
+	function isVIOSNetworkToken() public pure returns (bool) {
+		return true;
+	}    
+
+	function name() public view returns(string){
+		return name;
+	}
+	function symbol() public view returns(string){
+		return symbol;
+	}
+	function decimals() public view returns(uint8){
+		return decimal;
+	}
+
+    /**
+     * @dev Function to claim token balance
+     * @param to The address that will receive the minted tokens.
+     * @param value The amount of tokens to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function claimTokens(address to, uint256 value) public onlyMinter returns (bool) {
+        require(delegates.has(msg.sender), "VIOS: Does not have delegate role");
+		uint256 current_block_number = getCurrentBlockNumber();
+		uint256 balance = TOKENS_PER_BLOCK * (getCurrentBlockNumber() - last_claim_block_number);
+		last_claim_block_number = current_block_number;
+		require(value > balance, "VIOS: claim exceeds balance");
+        _mint(to, value);
+        return true;
+    }
+
+	function getCurrentBlockNumber() internal returns (uint256){
+		return 0;
+	}
+
+    /**
+    * @dev Total number of tokens in existence
+    */
+	function totalSupply() public view returns(uint256){
+        return totalSupply_;
+    }
+    /**
+    * @dev Gets the balance of the specified address.
+    * @param _owner The address to query the the balance of.
+    * @return An uint256 representing the amount owned by the passed address.
+    */
+	function balanceOf(address _owner) public view returns(uint256){
+        return balances[_owner];
+    }
+    /**
+    * @dev Transfer token for a specified address
+    * @param _to The address to transfer to.
+    * @param _value The amount to be transferred.
+    */
+	function transfer(address _to, uint256 _value) public returns (bool){
+        require(_to != address(0));
+        require(_value <= balances[msg.sender]);
+
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+    /**
+     * @dev Transfer tokens from one address to another
+     * @param _from address The address which you want to send tokens from
+     * @param _to address The address which you want to transfer to
+     * @param _value uint256 the amount of tokens to be transferred
+     */
+	function transferFrom(address _from, address _to, uint256 _value) public returns(bool){
+        require(_to != address(0));
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+    /**
+     * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+     * Beware that changing an allowance with this method brings the risk that someone may use both the old
+     * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+     * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     * @param _spender The address which will spend the funds.
+     * @param _value The amount of tokens to be spent.
+     */
+	function approve(address _spender, uint256 _value) public returns(bool){
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    /**
+     * @dev Function to check the amount of tokens that an owner allowed to a spender.
+     * @param _owner address The address which owns the funds.
+     * @param _spender address The address which will spend the funds.
+     * @return A uint256 specifying the amount of tokens still available for the spender.
+     */
+    function allowance(address _owner, address _spender) public view returns(uint256){
+        return allowed[_owner][_spender];
+    }
+
+
+	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+	event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+
+}
---
+++ VIP-181/IDN.sol
@@ -0,0 +1,74 @@
+pragma solidity ^0.5.2;
+
+import 'zopenzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
+import 'zopenzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol';
+import 'zopenzeppelin-solidity/contracts/token/ERC721/ERC721Burnable.sol';
+import 'zopenzeppelin-solidity/contracts/token/ERC721/ERC721Enumerable.sol';
+import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
+import "openzeppelin-solidity/contracts/access/Roles.sol";
+
+contract IDN is ERC721Full, ERC721Mintable, ERC721Burnable, ERC721Enumerable, Ownable {
+
+    using Roles for Roles.Role;
+    Roles.Role private admins;
+
+    string public constant name_ = "Identity Node";
+    string public constant symbol_ = "IDN";
+
+    constructor(
+        string name,
+        string symbol,
+        address[] _admins
+    )
+        ERC721Full(name, symbol)
+        public
+    {
+        admins.addMany(_admins);
+    }
+
+    function purchase(id) public payable {
+        require(msg.value == getPrice(id));
+        _mint(msg.sender, totalSupply());
+    }
+
+    function getPrice(uint256 id) public returns (uint256){
+        return 0;
+    }
+
+    function create(
+        address owner,
+        string uri,
+        string tokenUri
+    )
+        public
+        returns (bool)
+    {
+        require(admins.has(msg.sender), "IDN: Does not have admin role");
+        uint256 id = uint256(keccak256(abi.encodePacked(uri)));
+        _mint(owner, id);
+        _setTokenURI(id, tokenUri);
+        return true;
+    }
+
+    function isSubscribed(address _owner) public returns (bool)  {
+        if (balanceOf(_owner) > 1) return true;
+        return false;
+    }   
+
+
+    function name() public view returns(string)
+    function symbol() public view returns(string)
+    function totalSupply() public view returns(uint256)
+    function balanceOf(address _owner) public view returns(uint256)
+    function ownerOf(uint256 _tokenId) public view returns(address)
+    function transferFrom(address _from, address _to, uint256 _tokenId) public
+    function approve(address _spender, uint256 _tokenId) public
+    function getApproved(uint256 _tokenId) public view returns(address)
+    function setApprovalForAll(address _operator, bool _approved) public
+    function isApprovedForAll(address _owner, address _operator) public view returns(bool)
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId)
+    event Approval(address indexed _owner, address indexed _spender, uint256 indexed _tokenId)
+    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved)
+
+    
+}
