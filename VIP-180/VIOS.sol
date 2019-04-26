445061926434a64526329a93e1b68203ac83a569
Sherman Monroe <shermanmonroe@Shermans-MacBook-Air.local>
2019-04-26 12:55:19 -0500

    Vote logic
    - fixed vote logic
    - code refactoring

--- VIP-180/VIOS.sol
+++ VIP-180/VIOS.sol
@@ -12,8 +12,12 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
  */
 contract VIOS is ERC20, ERC20Detailed {
     using Roles for Roles.Role;
-    Roles.Role private trustees;
+    Roles.Role public trustees;
 
+    /**
+     * @dev The Authority Node is owned by a multisig wallet that requires 21 signatures. These signers are the Authority Nodes.
+     */
+    ATN public auth = ATN(); // TODO: insert ATN address
 
     uint8 public constant DECIMALS = 18;
     uint256 public constant INITIAL_SUPPLY = 100000000 * (10 ** uint256(DECIMALS));
@@ -24,7 +28,7 @@ contract VIOS is ERC20, ERC20Detailed {
     string public symbol = "VIOS";
     uint8 public decimals = DECIMALS;
     uint8 public last_claim_block_number = 0;
-    uint256 private _cap;
+    uint256 public _cap;
 
 
     mapping(address => uint256) balances;
@@ -40,8 +44,9 @@ contract VIOS is ERC20, ERC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public {
+    constructor (ATN _auth) public {
     	_cap = MAX_SUPPLY;
+        auth = _auth;
         _mint(msg.sender, INITIAL_SUPPLY);
     }
 
@@ -161,19 +166,17 @@ contract VIOS is ERC20, ERC20Detailed {
 
 
     // ********* the ANDREW functions ***********//
-    uint256 public constant DIVISOR_SIMPLE_MAJORITY = 2;
     uint public constant VOTE_PROPOSAL_ADD_TRUSTEE = 0;
     uint public constant VOTE_PROPOSAL_REMOVE_TRUSTEE = 1;
+    uint public constant BALLOT_STATUS_NONE = 0;
+    uint public constant BALLOT_STATUS_DEFAULT = 1;
+    uint public constant BALLOT_STATUS_VOTED = 2;
+    uint public constant BALLOT_STATUS_DELEGATED = 3;
     uint public constant DELEGATE_CHAIN_LIMIT = 10;
-    uint public constant  SET_AUTH_WAIT = 1000000000000;
-       
-    /**
-     * @dev The Authority Node is owned by a multisig wallet that requires 21 signatures. These signers are the Authority Nodes.
-     */
-    ATN auth = ATN(); // TODO: insert ATN address
-
+    uint public constant  SET_AUTH_WAIT = 1000000000000; //TODO
     struct voter {
         uint256 credits; // credits is accumulated by delegation
+        uint status;
         address delegate;   // the person that the voter chooses to delegate their vote to instead of voting themselves
     }
     // This is a type for a single proposal.
@@ -181,10 +184,13 @@ contract VIOS is ERC20, ERC20Detailed {
         address trusteeNominee;
         uint yay; // the number of positive votes accumulated
         uint nay; // the number of negative votes accumulated
+        uint type;
         uint authorizedYay;
         uint authorizedNay;
         bool authorized;
     }
+
+    uint public majorityDivisor = 2;
 
     // A dynamically-sized array of 'Proposal' structs.
     Proposal[] public proposals;
@@ -198,7 +204,7 @@ contract VIOS is ERC20, ERC20Detailed {
         auth = newAuth;
     }
 
-    uint set_auth_block_number; // the Authority has 8 hours to revert the setAuth change
+    uint public set_auth_block_number; // the Authority has 8 hours to revert the setAuth change
     function setAuthByCommunity(uint nominateeIndex){
         // TODO: attach a high price so that this function does not get spammed
         require(trustees.has(msg.sender), "ANDREW: sender does not have trustee role");
@@ -218,18 +224,24 @@ contract VIOS is ERC20, ERC20Detailed {
     }
 
     /// New poll for choosing one of the 'trustees' nominees
-    function create(address[] trusteeNominees) {
+    function create(address[] trusteeNominees, uint[] types, uint _majorityDivisor) {
         require(auth.isSubscribed(msg.sender) || proposals.length == 0, 'ANDREW: poll in progress');
+        require(_majorityDivisor > 5, 'ANDREW: requires 20% participation or more');
+        require(_majorityDivisor < 2, 'ANDREW: requires 50% participation or less');
+        require(trusteeNominees.length == types.length, 'ANDREW: invalid proposal');
         // For every provided trustee, a new proposal object is created and added to the array's end.
         delete proposals;
         delete voters;
+        majorityDivisor = _majorityDivisor;
         for (uint i = 0; i < trusteeNominees.length; i++) {
             // 'Proposal({...})' will create a temporary Proposal object 
             // 'proposals.push(...)' will append it to the end of 'proposals'.
+            require(type[i] == VOTE_PROPOSAL_REMOVE_TRUSTEE || type[i] = VOTE_PROPOSAL_ADD_TRUSTEE, 'ANDREW: invalid proposal');
             proposals.push(Proposal({
                 trusteeNominee: trusteeNominees[i],
                 yay: 0,
                 nay: 0,
+                type: type[i],
                 authorizedYay: 0,
                 authorizedNay: 0,
                 authorized: false
@@ -241,6 +253,7 @@ contract VIOS is ERC20, ERC20Detailed {
         require(auth.isSubscribed(msg.sender), 'ANDREW: denied by Authority');
         delete proposals;
         delete voters;
+        majorityDivisor = 2;
     }
 
     function issueBallot(address voter) {
@@ -254,12 +267,13 @@ contract VIOS is ERC20, ERC20Detailed {
         if(auth.isSubscribed(voter)){
 
             // the auth can alternatively call doVote directly
-            voters[voter].credits = 0;
+            voters[voter].credits = BALLOT_STATUS_NONE;
             // the Authority address is not allowed to vote
         }
         else{
-            require(voters[voter].credits == 0, 'ANDREW: ballot already exists');
+            require(voters[voter].status == BALLOT_STATUS_NONE, 'ANDREW: ballot already exists');
             voters[voter].credits = balanceOf(voter);
+            voters[voter].status = BALLOT_STATUS_DEFAULT;
         }
     }
 
@@ -271,8 +285,9 @@ contract VIOS is ERC20, ERC20Detailed {
         // Self-delegation is not allowed.
         require(to != msg.sender, 'ANDREW: self-delegation not allowed');
         voter storage sender = voter[msg.sender];
-        require (sender.credits > -1, 'ANDREW: no credits');
-        require (sender.credits > 0, 'ANDREW: no ballot');
+        require (sender.status != BALLOT_STATUS_DELEGATED, 'ANDREW: credits delegated');
+        require (sender.status != BALLOT_STATUS_VOTED, 'ANDREW: vote already cast');
+        require (sender.status != BALLOT_STATUS_NONE, 'ANDREW: no ballot');
 
         // Forward the delegation as long as
         // 'to' is delegated too.
@@ -294,15 +309,17 @@ contract VIOS is ERC20, ERC20Detailed {
         // Since 'sender' is a reference, this will modify 'sender.ballot[nominatedTrustee].credits'
         sender.delegate = delegateAddr;
         voter storage delegate = voter[delegateAddr];
-        if(delegate.credits < 0) delegate.credits = 0; // allow delegate to resume voting if credits are furnished after her vote
+        if(delegate.credits == 0) delegate.credits = 0; // allow delegate to resume voting if credits are furnished after her vote
         delegate.credits += sender.credits;
-        sender.credits = -2;
+        sender.status = BALLOT_STATUS_DELEGATED;
     }
 
     function authorize(uint nominateeIndex, uint yayOrNay){
+        uint yay = proposals[nominateeIndex].yay;
+        uint nay = proposals[nominateeIndex].nay;
         require(auth.isSubscribed(msg.sender), 'ANDREW: sender is not Authority');
-        if(yayOrNay == 1) proposals[nominateeIndex].authorizedYay = proposals[nominateeIndex].yay;
-        else if(yayOrNay == 0) proposals[nominateeIndex].authorizedNay = proposals[nominateeIndex].nay;
+        if(yayOrNay == 1) proposals[nominateeIndex].authorizedYay = yay;
+        else if(yayOrNay == 0) proposals[nominateeIndex].authorizedNay = nay;
         proposals[nominateeIndex].authorized = true;
     }
 
@@ -314,38 +331,40 @@ contract VIOS is ERC20, ERC20Detailed {
     }
 
     /// Cast a vote or veto (including votes delegated to you) for nominatedTrustee
-    function vote(uint nominateeIndex, bool yay) {
+    function vote(uint nominateeIndex, bool yay, uint type) {
         // If 'voter' or 'nominatedTrustee' are out of the range of the array,
         // this will throw automatically and revert all
         // changes.
         
         voter storage sender = voter[msg.sender];
-        require (sender.credits != -2, 'ANDREW: credits delegated');
-        require (sender.credits != -1, 'ANDREW: vote already cast');
-        require (sender.credits != 0, 'ANDREW: no ballot');
+        require (sender.status != BALLOT_STATUS_DELEGATED, 'ANDREW: credits delegated');
+        require (sender.status != BALLOT_STATUS_VOTED, 'ANDREW: vote already cast');
+        require (sender.status != BALLOT_STATUS_NONE, 'ANDREW: no ballot');
+        require (proposals[nominateeIndex].type != type, 'ANDREW: type not found'); // ensure the sender knows which proposal type they are voting on
         if(yay) proposals[nominateeIndex].yay += sender.delegateWeight;
         else proposals[nominateeIndex].nay += sender.delegateWeight;
-        sender.credits = -1;
+        sender.status = BALLOT_STATUS_VOTED;
     }
 
     /// @dev Attempts to assign the trustee
-    function execute(uint nominateeIndex, bool yay) constant
+    function execute(uint nominateeIndex, bool yay, uint type) constant
             returns (bool)
     {
-        require(proposals[nominateeIndex].authorized, 'ANDREW: denied by Authority');
-        uint tally = 0;
-        if(yay) tally = proposals[nominateeIndex].authorizedYay;
-        else tally = proposals[nominateeIndex].authorizedNay;
-        if(tally > div (totalSupply(), DIVISOR_SIMPLE_MAJORITY) ){
-            address trusteeNominee = proposals[nominateeIndex].trusteeNominee;
-            if(yay){
-                trustees.add(nominatedTrustee);
+        Proposal proposal = proposals[nominateeIndex];
+        require(proposal.authorized, 'ANDREW: denied by Authority');
+        require (proposal.type != type, 'ANDREW: type not found');
+        uint total = proposal.authorizedYay;
+        total += proposal.authorizedNay;
+        if(total >= div (totalSupply(), uint256(majorityDivisor)) && proposal.authorizedYay > proposal.authorizedNay){
+            if(type == VOTE_PROPOSAL_ADD_TRUSTEE){
+                trustees.add(proposal.trusteeNominee);
             }
-            else {
-                trustees.remove(nominatedTrustee);                
+            else if(type == VOTE_PROPOSAL_REMOVE_TRUSTEE) {
+                trustees.remove(proposal.trusteeNominee);                
             }
             delete proposals;
             delete voters;
+            majorityDivisor = 2;
             return true;
         }
         // TODO: place penalty here
